64aa305a1177cd48d687dbc4d3a5e499f4ce749a
Leks <alexey.mishkel@gmail.com>
2023-10-02 17:57:25 +0300

    reformated code

--- src/main/java/ru/netology/services/freelance.java
+++ src/main/java/ru/netology/services/freelance.java
@@ -2,22 +2,22 @@ package ru.netology.services;
 
 public class freelance {
 
-    public int calculate (int income,int expenses,int threshold) {
-    int money = 0;
-    int counter = 0;
-    for (int i = 0; i < 12; i++) {
-        if (money >= threshold){
-            //Фрилансер ушел в отпуск
-            money = money - expenses;
-            money = money / 3;
-            counter = counter +1;
-        }else{
-            //Фрилансер работает
-            money = money + income;
-            money = money - expenses;
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int counter = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                //Фрилансер ушел в отпуск
+                money = money - expenses;
+                money = money / 3;
+                counter = counter + 1;
+            } else {
+                //Фрилансер работает
+                money = money + income;
+                money = money - expenses;
+            }
         }
-    }
-    return counter;
+        return counter;
     }
 
 }
--- src/test/java/ru/netology/services/freelanceTest.java
+++ src/test/java/ru/netology/services/freelanceTest.java
@@ -1,26 +1,28 @@
 package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class freelanceTest {
-   @Test
+    @Test
 
-    public void testCalculate1 () {
-       freelance service = new freelance();
-       int actual = service.calculate(10_000, 3_000, 20_000);
-       int expected = 3;
+    public void testCalculate1() {
+        freelance service = new freelance();
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
 
-       Assertions.assertEquals(expected, actual);
-   }
-   @Test
+        Assertions.assertEquals(expected, actual);
+    }
 
-   public void testCalculate2 () {
-      freelance service = new freelance();
-      int actual = service.calculate(100_000, 60_000, 150_000);
-      int expected = 2;
+    @Test
 
-      Assertions.assertEquals(expected, actual);
-   }
+    public void testCalculate2() {
+        freelance service = new freelance();
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
